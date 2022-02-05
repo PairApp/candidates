05770ebd5be45be0b25c4e96a3fab5fdc3e0d584
PairApp <79168736+PairApp@users.noreply.github.com>
2022-02-05 15:12:51 -0800

    Update Follower.js

--- src/Follower.js
+++ src/Follower.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
-const Follower = ({ avatar_url, html_url, login }) => {
+const Follower = () => {
   return (
     <article className='card'>
-      <img src={avatar_url} alt={login} />
-      <h4>${login}</h4>
-      <a href={html_url} className='btn'>
-        view profile
-      </a>
+     
     </article>
   )
 }
